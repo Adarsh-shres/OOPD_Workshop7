f245f462e0ec8a60de6ca9000e0d64175e587a76
Adarsh-shres <ansbm3299@gmail.com>
2026-01-05 19:59:26 +0545

    Added getter methods to Student class

--- src/Student.java
+++ src/Student.java
@@ -2,5 +2,15 @@ public class Student {
     String name;
     int age;
 
-public Student(){}
+public Student(String name, int age){
+    this.name = name;
+    this.age = age;
+}
+
+public String getName() {
+    return name;
+}
+public int getAge() {
+    return age;
+}
 }
